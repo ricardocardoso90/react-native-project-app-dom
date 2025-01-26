ede3eea0a1a8f13cf12cd0716cb666acb0e86815
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-01-25 21:48:58 -0300

    update commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 32 }}>
+      <Text>Olá Mundão</Text>
+    </View>
+  )
+};
